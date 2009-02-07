9196d687a639cc42ba3d4f10d65a20b08480b6a6
deven.phillips <deven.phillips@67d09a9e-e402-11dd-9ba5-89a75009fd5d>
2009-02-07 12:46:06 +0000

    Added logic to allow the user to specify the location in which to store the lucene indexes during the initial configuration. Still need to debug this because attempts to change the location after an initial failure also fail.

--- Triage/src/com/blogspot/devenphillips/helpdesk/data/User.java
+++ Triage/src/com/blogspot/devenphillips/helpdesk/data/User.java
@@ -9,14 +9,14 @@ import org.hibernate.search.annotations.* ;
 @Entity
 @Table(name="users")
 @Indexed
-@SequenceGenerator(name="device_seq", sequenceName="devices_id_seq")
+@SequenceGenerator(name="user_seq", sequenceName="user_id_seq")
 public class User implements java.io.Serializable {
 
 	private static final long serialVersionUID = -3946375223199150445L;
 
 	@Id
 	@DocumentId
-	@GeneratedValue(generator="device_seq",strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(generator="user_seq",strategy=GenerationType.SEQUENCE)
 	private int userId = 0 ;
 
 	@Field(index=Index.TOKENIZED, store=Store.NO)
@@ -28,56 +28,91 @@ public class User implements java.io.Serializable {
 	private String workphone = null ;
 	private String mobile = null ;
 	private String email = null ;
+	private String password = null ;
 
 	@ManyToMany( cascade = { CascadeType.PERSIST, CascadeType.REMOVE } )
 	@JoinTable(name="usergroups",joinColumns={@JoinColumn(name="userId",unique=false)},inverseJoinColumns={@JoinColumn(name="groupId",unique=false)})
 	@IndexedEmbedded
 	private Set<Group> groups = null ;
 
+	@OneToMany( cascade = { CascadeType.PERSIST, CascadeType.REMOVE } )
+	@JoinColumn(name="userId")
+	private Set<Preference> preferences = null ;
+
 	public String getForename() {
 		return forename;
 	}
+
 	public void setForename(String forename) {
 		this.forename = forename;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 	public String getNotes() {
 		return notes;
 	}
+
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
+
 	public String getWorkphone() {
 		return workphone;
 	}
+
 	public void setWorkphone(String workphone) {
 		this.workphone = workphone;
 	}
+
 	public String getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getRecord() {
 		return userId;
 	}
+
 	public void setGroups(Set<Group> groups) {
 		this.groups = groups;
 	}
+
 	public Set<Group> getGroups() {
 		return groups;
 	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean checkPassword(String password) {
+		// TODO: Check encrypted password VS database password.
+		return true ;
+	}
+
+	public void setPreferences(Set<Preference> preferences) {
+		this.preferences = preferences;
+	}
+
+	public Set<Preference> getPreferences() {
+		return preferences;
+	}
 }
